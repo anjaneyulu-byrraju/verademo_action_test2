564a881a2f2816c64ef83914b34ccdc40ade519d
anji-automation <anjaneyulu.by6@gmail.com>
2024-07-18 12:21:42 +0530

    second commit from branch6

--- tests/src/main/java/com/veracode/verademo/commands/ListenCommand.java
+++ tests/src/main/java/com/veracode/verademo/commands/ListenCommand.java
@@ -47,7 +47,7 @@ public class ListenCommand implements BlabberCommand {
 			sqlStatement.execute(sqlQuery);
 			/* END EXAMPLE VULNERABILITY */
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block test
+			// TODO Auto-generated catch block testttt
 			e.printStackTrace();
 		}
 	}
