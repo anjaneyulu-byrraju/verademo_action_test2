c87829a1a67996f9c0b73e7046b5f30f2f813576
anji-automation <anjaneyulu.by6@gmail.com>
2024-07-18 10:28:34 +0530

    commit for the changed fix froma branch4

--- tests/src/main/java/com/veracode/verademo/commands/IgnoreCommand.java
+++ tests/src/main/java/com/veracode/verademo/commands/IgnoreCommand.java
@@ -40,14 +40,14 @@ public class IgnoreCommand implements BlabberCommand {
 			ResultSet result = sqlStatement.executeQuery(sqlQuery);
 			result.next();
 
-			/* START EXAMPLE VULNERABILITY */
+			/* START EXAMPLE VULNERABILITY Test */
 			String event = username + " is now ignoring " + blabberUsername + " (" + result.getString(1) + ")";
 			sqlQuery = "INSERT INTO users_history (blabber, event) VALUES (\"" + username + "\", \"" + event + "\")";
 			logger.info(sqlQuery);
 			sqlStatement.execute(sqlQuery);
 			/* END EXAMPLE VULNERABILITY */
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
+			// TODO Auto-generated catch block test
 			e.printStackTrace();
 		}
 	}
